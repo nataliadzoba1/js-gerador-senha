7a3ea8d6e88ce5d8a3e335a8ed6d27add470c088
natalia dzoba <145125394+nataliadzoba1@users.noreply.github.com>
2025-08-07 10:49:17 +0000

    random

--- main.js
+++ main.js
@@ -2,21 +2,33 @@ const numeroSenha = document.querySelector('.parametro-senha__texto');
 let tamanhoSenha = 12;
 numeroSenha.textContent = tamanhoSenha;
 
-const botoes = document.querySelector('.parametro-senha__botao');
+const botoes = document.querySelectorAll('.parametro-senha__botao');
 
-botoes{0}.onclick =diminuiTamanho;
-botoes{1}.onclick =aumentaTamanho;
+botoes[0].onclick = diminuiTamanho;
+botoes[1].onclick = aumentaTamanho;
 
 function diminuiTamanho(){
     if (tamanhoSenha > 1){
-    tamanhoSenha = tamanhoSenha - 1;
+     tamanhoSenha = tamanhoSenha - 1;
     }
     numeroSenha.textContent = tamanhoSenha;
 }
 
 function aumentaTamanho(){
     if (tamanhoSenha < 20){
-    tamanhoSenha = tamanhoSenha + 1;
+     tamanhoSenha = tamanhoSenha + 1;
     }
     numeroSenha.textContent = tamanhoSenha;
+}
+
+const campoSEnha = document.querySelector('#campo-senha');
+
+const letrasMaiusculas = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ'
+geraSenha();
+
+function geraSenha();{
+    let senha = '';
+    for (let i = 0 ; i < tamanhoSenha; i++){
+        let numeroAleatorio = Math.random() * letrasMaiusculas.length;
+    }
 }
