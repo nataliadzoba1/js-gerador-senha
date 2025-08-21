7a92ecd73ce21197fa71877f3770aefaf914669a
natalia dzoba <145125394+nataliadzoba1@users.noreply.github.com>
2025-08-21 10:31:17 +0000

    atrasados

--- main.js
+++ main.js
@@ -9,26 +9,60 @@ botoes[1].onclick = aumentaTamanho;
 
 function diminuiTamanho(){
     if (tamanhoSenha > 1){
-     tamanhoSenha = tamanhoSenha - 1;
+      tamanhoSenha = tamanhoSenha - 1;
     }
     numeroSenha.textContent = tamanhoSenha;
+    geraSenha();
 }
 
 function aumentaTamanho(){
     if (tamanhoSenha < 20){
-     tamanhoSenha = tamanhoSenha + 1;
+      tamanhoSenha = tamanhoSenha + 1;
     }
     numeroSenha.textContent = tamanhoSenha;
+    geraSenha();
 }
 
-const campoSEnha = document.querySelector('#campo-senha');
+const campoSenha = document.querySelector('#campo-senha');
+const checkbox = document.querySelectorAll('.checkbox');
 
-const letrasMaiusculas = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ'
+for(let i = 0; i < checkbox.length; i++){
+    checkbox[i].onclick = geraSenha;
+}
+
+const letrasMaiusculas = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ';
+const letrasMinusculas = 'abcdefghijklmnopqrstuvwxyz';
+const numeros = '0123456789';
+const simbolos = '!@%*?';
+
+
 geraSenha();
 
-function geraSenha();{
+function geraSenha(){
+    let alfabeto = '';
+    if (checkbox[0].checked){
+        alfabeto = alfabeto + letrasMaiusculas;
+    }
+    if (checkbox[1].checked){
+        alfabeto = alfabeto + letrasMinusculas;
+    }
+    if (checkbox[2].checked){
+        alfabeto = alfabeto + numeros;
+    }
+    if (checkbox[3].checked){
+        alfabeto = alfabeto + simbolos;
+    }
+    
+    // Adicione esta verificação para garantir que o alfabeto não esteja vazio
+    if (alfabeto === '') {
+        alfabeto = letrasMaiusculas + letrasMinusculas;
+    }
+
     let senha = '';
     for (let i = 0 ; i < tamanhoSenha; i++){
-        let numeroAleatorio = Math.random() * letrasMaiusculas.length;
+        let numeroAleatorio = Math.random() * alfabeto.length;
+        numeroAleatorio = Math.floor(numeroAleatorio);
+        senha = senha + alfabeto[numeroAleatorio];
     }
+    campoSenha.value = senha;
 }
